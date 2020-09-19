8a4b8328389742c540621018caa61f84abc23b32
Miguel Ángel Medina Ramírez <miguelangel.kjh@gmail.com>
2020-09-19 14:27:29 +0100

    add CRUD of products

---
+++ src/main/java/guro/springframework/controllers/ProductController.java
@@ -0,0 +1,58 @@
+package guro.springframework.controllers;
+
+import guro.springframework.domain.Product;
+import guro.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProduct(Model model){
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+        return "product";
+    }
+
+    @RequestMapping("/product/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+        return "productForm";
+    }
+
+    @RequestMapping("/product/new")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "productForm";
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public String saveOrUpdateProduct(Product product) {
+        Product saveProduct = productService.saveOrUpdateProduct(product);
+        return "redirect:/products";
+    }
+
+    @RequestMapping("/product/delete/{id}")
+    public String delete(@PathVariable Integer id){
+        productService.deleteProduct(id);
+
+        return "redirect:/products";
+    }
+}
---
+++ src/main/java/guro/springframework/services/ProductService.java
@@ -0,0 +1,16 @@
+package guro.springframework.services;
+
+import guro.springframework.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveOrUpdateProduct(Product product);
+
+    void deleteProduct(Integer id);
+
+}
